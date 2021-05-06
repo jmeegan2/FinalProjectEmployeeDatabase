39752f4bf917b04641eef8520a649a59005c8a33
jmeegan2 <jmeegan2@owlmail.harford.edu>
2021-05-05 23:40:57 -0400

    x

--- Source.cpp
+++ Source.cpp
@@ -30,7 +30,7 @@ public:
             cin >> socialSecurityNumber; //at the present moment its only accepting social security numbers that are 6 digits xxx-xxx would be an example
         } while (strchr(socialSecurityNumber, '-') == NULL);
 
-        cout << "Salary: ";
+        cout << "Salary (no comma needed): ";
         cin >> salary;
         cout << "Age: ";
         cin >> age;
